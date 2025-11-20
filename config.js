eed0afd5dd3d547afe17f51c7c93c287c4564418
Stuart Colville <scolville@mozilla.com>
2025-11-20 19:19:24 +0000

    Update exclusions and props list

--- config.js
+++ config.js
@@ -206,6 +206,8 @@ export default {
     'inset-inline-start',
     'gap',
     'grid-gap', // Deprecated in favour of gap but still used.
+    'row-gap',
+    'column-gap',
     'margin',
     'margin-block',
     'margin-block-end',
@@ -219,6 +221,7 @@ export default {
     'margin-left',
     'opacity',
     'outline',
+    'outline-color',
     'outline-offset',
     'padding',
     'padding-block',
@@ -231,7 +234,6 @@ export default {
     'padding-right',
     'padding-bottom',
     'padding-left',
-    'row-gap',
   ],
   designTokenKeys,
   // Globs to find CSS to get design token propagation data for.
@@ -274,6 +276,10 @@ export default {
   //    if you don't want a wildcard value to match a specific property.
   excludedDeclarations: [
     {
+      property: 'margin',
+      values: ['0 auto', 'auto 0'],
+    },
+    {
       property: 'font-weight',
       values: ['normal'],
     },
@@ -288,6 +294,8 @@ export default {
         'inherit',
         'initial',
         'none',
+        'revert',
+        'revert-layer',
         'transparent',
         'unset',
         /calc(.*?)/,
